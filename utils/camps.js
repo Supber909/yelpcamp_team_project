f5a5701ad7151f3724dd65c4abdae450a597bd71
Felicity Essien <felicityabel2016@gmail.com>
2023-09-16 12:01:57 +0100

    added json data for houses

---
+++ utils/camps.js
@@ -0,0 +1,145 @@
+const camps = [
+    {
+      id: 1,
+      image: "../utils/assets/images/compressed/mountUlap.jpg",
+      name: 'mount ulap',
+      price:"$360/hr",
+      description: 'one of the most famous hikes in Benguet is Mt Ulap in Itogon.',
+      reviews: [
+        {
+            id: 1,
+            review: "A really nice place",
+            name: "john Amos",
+            time:"12hrs ago"
+
+        },
+        {
+            id: 2,
+            review: "A really nice place, you should visit",
+            name: "Andre Pete",
+            time:"12hrs ago"
+
+        }
+      ]
+    },
+    {
+        id: 2,
+        name: 'Calaguas Islands',
+        price:"$260/hr",
+        description: 'A paradise of islands that can rival the white sand and beauty of Boracay',
+        reviews: [
+          {
+              id: 1,
+              review: "I was there and i'll advice you to go, its a great way to connect with nature",
+              name: "Daphne Yemi",
+              time:"8hrs ago"
+  
+          },
+          {
+            id: 2,
+            review: "Go connect with nature at this wonderful place",
+            name: "Patience Peter",
+            time:"19hrs ago"
+
+        }
+        ]
+      },
+      {
+        id: 3,
+        name: 'Onay Beach',
+        price:"$560/hr",
+        description: 'This is one of the best beach camping sites, beautiful and pristine',
+        reviews: [
+          {
+              id: 1,
+              review: "I was there and i'll advice you to go, its a great way to connect with nature",
+              name: "Daphne Yemi",
+              time:"8hrs ago"
+  
+          },
+          {
+            id: 2,
+            review: "I was there and i'll advice you to go, its a great way to connect with nature",
+            name: "Daphne Yemi",
+            time:"8hrs ago"
+
+        },
+        {
+            id: 3,
+            review: "I was there and i'll advice you to go, its a great way to connect with nature",
+            name: "Daphne Yemi",
+            time:"8hrs ago"
+
+        }
+        ]
+      },
+      {
+        id: 4,
+        name: 'Seven Sisters Waterfall',
+        price:"$660/hr",
+        description: 'The seven sisters is the 39th tallest waterfall in Norway',
+        reviews: [
+          {
+              id: 1,
+              review: "I was there and i'll advice you to go, its a great way to connect with nature",
+              name: "Daphne Yemi",
+              time:"8hrs ago"
+  
+          },
+          {
+            id: 2,
+            review: "Go connect with nature at this wonderful place",
+            name: "Patience Peter",
+            time:"19hrs ago"
+
+        }
+        ]
+      },
+      {
+        id: 5,
+        name: 'Latik Riverside',
+        price:"$360/hr",
+        description: 'Philippines is one of the most dazzling countries in Asia',
+        reviews: [
+          {
+              id: 1,
+              review: "A really nice place",
+              name: "Blessing Amos",
+              time:"12hrs ago"
+  
+          },
+           {
+              id: 2,
+              review: "A really nice place",
+              name: "Felicity Abel",
+              time:"7hrs ago"
+  
+          }
+        ]
+      }, 
+      {
+        id: 6,
+        name: 'Buloy Springs',
+        price:"$250/hr",
+        description: 'This is one of the best beach camping sites, beautiful and pristine',
+        reviews: [
+          {
+              id: 1,
+              review: "Beautiful and Pristine",
+              name: "Daphne Gee",
+              time:"12hrs ago"
+  
+          },
+           {
+              id: 2,
+              review: "A really nice place",
+              name: "Patience Duke",
+              time:"7hrs ago"
+  
+          }
+        ]
+      }, 
+  ];
+  
+  module.exports = camps;
+  
