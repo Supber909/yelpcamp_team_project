d490963057ea965caa017a1b38033a71604fe8dc
derhuv <daphnyib@gmail.com>
2023-09-20 00:10:38 +0100

    no camp functionality added

--- js/renderCamps.js
+++ js/renderCamps.js
@@ -65,7 +65,14 @@ searchBox.addEventListener('input', () => {
 function performSearch() {
   const searchText = searchBox.value.toLowerCase();
   const filteredCamps = searchCamps(searchText, camps);
-  renderCamps(filteredCamps);
+  
+  if (filteredCamps.length == 0) {
+    // Display "No results" message
+    campListContainer.innerHTML = "<h1 class='text-3xl lg:px-6 px-3'>No camp found</h1>";
+  } else {
+    // Call the renderCamps function with the filtered camps
+    renderCamps(filteredCamps);
+  }
 }
 
 // Get the search form element
@@ -73,6 +80,7 @@ const searchForm = document.getElementById('searchForm');
 
 // Add an event listener to the form's submit event
 searchForm.addEventListener('submit', (event) => {
+  
   event.preventDefault(); // Prevent the form from submitting
 
   // Get the search input value
@@ -82,12 +90,12 @@ searchForm.addEventListener('submit', (event) => {
   const filteredCamps = searchCamps(searchText, camps);
   // Call the renderCamps function with the filtered camps
 // Clear the camp details container
-campDetailsElement.innerHTML = '';
+campListContainer.innerHTML = '';
 
 // Check if there are search results
-if (filteredCamps.length <= 0) {
+if (filteredCamps.length == 0) {
   // Display "No results" message
-  campDetailsElement.innerHTML = "No results";
+  campListContainer.innerHTML = "<h1 class='text-3xl lg:px-6 px-3'>No camp found</h1>";
 } else {
   // Call the renderCamps function with the filtered camps
   renderCamps(filteredCamps);
